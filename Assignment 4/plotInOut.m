e75192b13050617dd567a409502e4dd019d17385
Frupla <s163920@student.dtu.dk>
2018-11-29 18:13:28 +0100

    :sweat_drops:

---
+++ Assignment 4/plotInOut.m
@@ -0,0 +1,21 @@
+function [H F] = plotInOut(filename, figureNumber)
+% Takes number in one of the input output files, plots it in figure number
+% figureNumber
+
+[in, fin] = import_sound(strcat('in_length_',filename,'.wav'));
+[out, fout] = import_sound(strcat('out_length_',filename,'.wav'));
+
+[In, Fin] = make_spectrum(in,fin);
+[Out, Fout] = make_spectrum(out,fout);
+
+H = Out./In;
+F = Fin;
+
+figure(figureNumber)
+subplot(2,1,1)
+plot(Fin,abs(In));
+subplot(2,1,2)
+plot(Fout,abs(Out));
+
+
+end
