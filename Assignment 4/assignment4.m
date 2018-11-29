5f50e350943939948800a352187e0d4b50af3d5b
Frupla <s163920@student.dtu.dk>
2018-11-29 16:23:44 +0100

    rykkede plot functionen

--- Assignment 4/assignment4.m
+++ Assignment 4/assignment4.m
@@ -43,29 +43,3 @@ figure(5)
 [A, fs] = spectre('animal_call.wav', 'hann', 99,1,100);
 imagesc(abs(A').^2);
 %I guess we could try filtering it with something?
-
-
-%% functions
-function [] = plotTwoSoundFilesAndCorrelation(namein, nameout, figurenumber1, figurenumber2)
-[in fin] = audioread(namein);
-[out fout] = audioread(nameout);
-tin = (0:length(in)-1)/fin;
-tout = (0:length(out)-1)/fout;
-
-figure(figurenumber1)
-subplot(2,1,1)
-plot(tin,in);
-title(namein)
-xlabel('Time (s)')
-subplot(2,1,2)
-plot(tout,out);
-title(nameout)
-xlabel('Time (s)')
-
-
-
-[r,lags] = xcorr(in,out);
-figure(figurenumber2)
-plot(lags,r)
-title('Correlation');
-end
